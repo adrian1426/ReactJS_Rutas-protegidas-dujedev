1ed88d13bcf55ae20fa2cdc1d41a8b3255f29aeb
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-03-01 14:34:04 -0600

    creando rutas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/products' element={<h1>Productos</h1>} />
+        <Route path='/cart' element={<h1>Carrito de compra</h1>} />
+        <Route path='*' element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
