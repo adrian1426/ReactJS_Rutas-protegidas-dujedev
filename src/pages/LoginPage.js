2f4a576c2f29ef3e1d8fa7e1f4e0b35ab99572f9
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-03-19 18:23:22 -0600

    usando useNavigate

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
+
 const LoginPage = (props) => {
   const { handleLogin } = props;
+  const navigate = useNavigate();
+
+  const _handleLogin = () => {
+    handleLogin();
+    navigate('/products');
+  };
 
   return (
     <div>
       <h2>Login</h2>
-      <button onClick={handleLogin}>Ingresar</button>
+      <button onClick={_handleLogin}>Ingresar</button>
     </div>
   );
 };
